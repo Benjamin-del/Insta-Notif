10aa6ed1f9b0904aa100c43184a35dd42237d9ee
Benjamin-del <ben@benja.ml>
2021-06-07 13:44:29 -0400

    Update notif.js
    Add comments

--- notif.js
+++ notif.js
@@ -1,15 +1,22 @@
+// COPY AN PASTE THIS IN TO /extras/notif.js !!!!!
+
+
+
 window.OneSignal = window.OneSignal || [];
   OneSignal.push(function() {
     OneSignal.init({
+      // ADD YOUR APP ID HERE
       appId: "YOUR_APP_ID_HERE",
+      // ADD YOUR SAFARI ID HERE
       safari_web_id: "YOUR_SAFARI_ID_HERE",
       notifyButton: {
         enable: true,
       },
+      // ADD YOUR SUBDOMAIN HERE
       subdomainName: "YOUR_SUB_DOMAIN",
     });
   });
-
+// THIS TRIGGERS THE SLIDE NOTIFICATION PROMPT! 
 OneSignal.push(function() {
   OneSignal.showSlidedownPrompt();
 });
