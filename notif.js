b34c314753d0d64f4d04d342334a7455aa25d0c5
Benjamin-del <67153251+Benjamin-del@users.noreply.github.com>
2021-06-07 10:14:44 -0400

    Update notif.js
    I added Safiri Support.

--- notif.js
+++ notif.js
@@ -2,6 +2,7 @@ window.OneSignal = window.OneSignal || [];
   OneSignal.push(function() {
     OneSignal.init({
       appId: "YOUR_APP_ID_HERE",
+      safari_web_id: "YOUR_SAFARI_ID_HERE",
       notifyButton: {
         enable: true,
       },
