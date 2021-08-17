92ceb85031d818d54f7b9b98e4e85ef7e2bffb91
Benjamin-del <ben@benja.ml>
2021-08-17 17:03:46 -0400

    Create new.js

---
+++ ex/new.js
@@ -0,0 +1,26 @@
+// DO NOT COPY IN TO /extras
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var NT = JSON.parse(this.responseText);
+
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+      OneSignal.init({
+        appId: NT.NOTIF_app_id,
+        //safari_web_id: NT.safari_web_id,
+        notifyButton: {
+          enable: true
+        },
+        //subdomainName: NT.subdomainName
+      });
+    });
+    // THIS TRIGGERS THE SLIDE NOTIFICATION PROMPT!
+    OneSignal.push(function() {
+      OneSignal.showSlidedownPrompt();
+    });
+  }
+};
+xmlhttp.open("GET", "/svr/svr.json", true);
+xmlhttp.send();
